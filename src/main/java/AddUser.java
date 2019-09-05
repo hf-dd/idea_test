e11a482c39736d16534bb1592dfbdb92e0afe69b
hf <admin@bjsxt.com>
2019-09-05 21:42:36 +0800

    第一次提交

---
+++ src/main/java/AddUser.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Auther:http://www.hf.com
+ * @Date:2019/9/5
+ * @Description:PACKAGE_NAME
+ * @version:1.0
+ */
+public class AddUser extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        String username = req.getParameter("username");
+        System.out.println(username);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+    <body>
+        <form action="addUser" method="post">
+        用户名：<input type="text" name="username"/><br/>
+        <input type="submit" value="OKOK"/>
+        </form>
+    </body>
+</html>
